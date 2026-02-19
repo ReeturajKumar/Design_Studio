53685f66541e98acd0ef4b944f77bd674c7334d5
ReeturajKumar <reeturajvats587@gmail.com>
2026-02-19 18:51:29 +0530

    Incresing Size

--- src/components/home/BoneShowcase.tsx
+++ src/components/home/BoneShowcase.tsx
@@ -3,17 +3,17 @@ import boneImg from "../../assets/Bone.webp"
 
 export default function BoneShowcase() {
   return (
-    <section className="bg-black pt-1 pb-10 md:pt-2 md:pb-15 lg:pt-3 lg:pb-15 px-6 md:px-12 lg:px-20 overflow-hidden">
+    <section className="bg-black pt-1 pb-10 md:pt-2 md:pb-15 lg:pt-3 lg:pb-15 px-0 md:px-12 lg:px-20 overflow-hidden">
       <div className="max-w-[1400px] mx-auto">
         <motion.div 
-          initial={{ opacity: 0, y: 80, scale: 0.8, filter: "blur(10px)" }}
+          initial={{ opacity: 0, y: 30, scale: 0.96, filter: "blur(10px)" }}
           whileInView={{ opacity: 1, y: 0, scale: 1, filter: "blur(0px)" }}
           viewport={{ once: false, amount: "some" }}
           transition={{ 
-            duration: 1.6, 
+            duration: 1.4, 
             ease: [0.22, 1, 0.36, 1] 
           }}
-          className="relative rounded-[32px] md:rounded-[48px] lg:rounded-[64px] overflow-hidden will-change-transform transform-gpu"
+          className="relative rounded-[0px] xs:rounded-[20px] md:rounded-[48px] lg:rounded-[64px] overflow-hidden will-change-transform transform-gpu"
         >
           <img 
             src={boneImg} 
